ac6d4e8da9cf7c1294e9eda16b9f8a24767b50b5
wandiabdullah <wandisyahid@gmail.com>
2020-05-17 12:33:46 +0700

    News App v.0.1
    Upload First
    1. Splash Screen
    2. Firebase login

---
+++ android/app/src/main/kotlin/id/pmo/newsappproject/MainActivity.kt
@@ -0,0 +1,9 @@
+package id.pmo.newsappproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'services/authentication.dart';
+import 'pages/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
